6b78e443802102e28459fa5b5b48663700ebfc45
Niharik Thakur <122886989+TNiharika123@users.noreply.github.com>
2023-07-03 22:02:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+ //typing animation script
+ var typed = new typed(".typing", {
+    strings: ["Student", "Front-end Developer", "Competitive Programmer" , "Community Evangelist"],
+    typeSpeed:40,
+    backSpeed:60,
+    loop:true
+ });
+ 
